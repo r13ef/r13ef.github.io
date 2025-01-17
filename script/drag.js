ecbf33c56e57d0a40b0522596c6472da34227905
r13ef <73205763+r13ef@users.noreply.github.com>
2025-01-17 12:45:12 +0900

    Major change

--- script/drag.js
+++ script/drag.js
@@ -1,4 +1,11 @@
-$orange_box.onpointermove = function (event) {
+console.log(document.getElementsByClassName('object'))
+for (object of document.getElementsByClassName('object')) {
+    top_int = parseInt(object.offsetTop)
+    left_int = parseInt(object.offsetLeft)
+    object.style.backgroundColor = 'rgb(' + top_int / 5 % 256 + ' ' + left_int / 5 % 256 + ' ' + (top_int + left_int) / 10 % 256 + ')'
+}
+
+box1.onpointermove = function (event) {
     if (event.buttons) {
         this.draggable = false
         this.style.position = 'absolute'
@@ -6,5 +13,36 @@ $orange_box.onpointermove = function (event) {
         this.style.left = this.offsetLeft + event.movementX + 'px'
         this.style.margin = 0 + 'px';
         this.setPointerCapture(event.pointerId)
+        top_int = parseInt(this.style.top)
+        left_int = parseInt(this.style.left)
+        this.style.backgroundColor = 'rgb(' + top_int / 5 % 256 + ' ' + left_int / 5 % 256 + ' ' + (top_int + left_int) / 5 % 256 + ')'
+    }
+}
+
+box2.onpointermove = function (event) {
+    if (event.buttons) {
+        this.draggable = false
+        this.style.position = 'absolute'
+        this.style.top = this.offsetTop + event.movementY + 'px'
+        this.style.left = this.offsetLeft + event.movementX + 'px'
+        this.style.margin = 0 + 'px';
+        this.setPointerCapture(event.pointerId)
+        top_int = parseInt(this.style.top)
+        left_int = parseInt(this.style.left)
+        this.style.backgroundColor = 'rgb(' + top_int / 5 % 256 + ' ' + left_int / 5 % 256 + ' ' + (top_int + left_int) / 5 % 256 + ')'
+    }
+}
+
+box3.onpointermove = function (event) {
+    if (event.buttons) {
+        this.draggable = false
+        this.style.position = 'absolute'
+        this.style.top = this.offsetTop + event.movementY + 'px'
+        this.style.left = this.offsetLeft + event.movementX + 'px'
+        this.style.margin = 0 + 'px';
+        this.setPointerCapture(event.pointerId)
+        top_int = parseInt(this.style.top)
+        left_int = parseInt(this.style.left)
+        this.style.backgroundColor = 'rgb(' + top_int / 5 % 256 + ' ' + left_int / 5 % 256 + ' ' + (top_int + left_int) / 5 % 256 + ')'
     }
 }
